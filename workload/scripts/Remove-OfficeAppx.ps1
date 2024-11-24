3c7b10e862d0e5032d9898e5f091b222b4cf46af
Joe Thompson <jwt0513@gmail.com>
2024-11-24 08:22:03 -0800

    Update Remove-OfficeAppx.ps1

--- workload/scripts/Remove-OfficeAppx.ps1
+++ workload/scripts/Remove-OfficeAppx.ps1
@@ -1,8 +1,29 @@
 # Remove consumer apps from Windows.
-$apps = 'Microsoft.OutlookForWindows', 'Microsoft.MicrosoftOfficeHub', 'MSTeams', 'Microsoft.BingWeather', 'Microsoft.MicrosoftSolitaireCollection', 'Microsoft.Xbox.TCUI', 'Microsoft.XboxIdentityProvider', 'Microsoft.XboxSpeechToTextOverlay', 'Microsoft.ZuneMusic'
+# https://woshub.com/how-to-uninstall-modern-apps-in-windows-10/
+$UWPAppstoRemove = @(
+    "Microsoft.OutlookForWindows", 
+    "Microsoft.MicrosoftOfficeHub", 
+    "MSTeams", 
+    "Microsoft.BingNews",
+    "Microsoft.BingWeather", 
+    "Microsoft.MicrosoftSolitaireCollection", 
+    "Microsoft.People",
+    "Microsoft.ScreenSketch",
+    "microsoft.windowscommunicationsapps",
+    "Microsoft.WindowsFeedbackHub",
+    "Microsoft.WindowsMaps"
+    "Microsoft.Xbox.TCUI", 
+    "Microsoft.XboxGameOverlay",
+    "Microsoft.XboxGamingOverlay",
+    "Microsoft.XboxIdentityProvider", 
+    "Microsoft.XboxSpeechToTextOverlay", 
+    "Microsoft.YourPhone",
+    "Microsoft.ZuneMusic",
+    "Microsoft.ZuneVideo"
+)
 
-foreach ($app in $apps) {
-    Write-Host "Processing app: $app"
-    # Perform actions on each app
-    Get-AppxPackage -Name $app -AllUsers | Remove-AppPackage -AllUsers
+# Remove preinstalled Microsoft Store applications for all users and from the Windows image
+foreach ($UWPApp in $UWPAppstoRemove) {
+    Get-AppxPackage -Name $UWPApp -AllUsers | Remove-AppxPackage -AllUsers -Verbose -ErrorAction silentlyContinue
+    Get-AppXProvisionedPackage -Online | Where-Object DisplayName -eq $UWPApp | Remove-AppxProvisionedPackage -Online -Verbose -ErrorAction silentlyContinue
 }
